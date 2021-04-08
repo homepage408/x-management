c2a7692cc9f84d88febf5d1c4025ce2189ab6e9f
Teguh Setiawan <homepage408@gmail.com>
2021-04-08 21:37:17 +0700

    task worker

--- app/schema/taskWorker.js
+++ app/schema/taskWorker.js
@@ -1,18 +1,14 @@
-const { gql } = require ("apollo-server-express")
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    extend type Query {
-        findAllTaskWorker (assignee : Int) : [Task]
-    }
+  extend type Query {
+    findAllTaskWorker(project_id: Int): [Task]
+  }
 
-    extend type Mutation {
-        isCheck(
-            id : Int
-            is_check : Boolean
-        ): Task
-    }
-
+  extend type Mutation {
+    isCheck(id: Int, is_check: Boolean): Task
+  }
 `;
 module.exports = {
-    typeDefs,
-}
+  typeDefs,
+};
